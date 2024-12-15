1db1829d78d6b46ec805990d6cd1db2f7d736556
Tembwe <temcoding600@gmail.com>
2024-12-15 19:01:05 +0000

    updated

---
+++ App.jsx
@@ -0,0 +1,50 @@
+// src/App.jsx
+
+import './App.css';
+import React, { useState } from 'react';
+import IngredientList from './components/IngredientList';
+import BurgerStack from './components/BurgerStack';
+
+export const availableIngredients = [
+  { name: 'Kaiser Bun', color: 'saddlebrown' },
+  { name: 'Sesame Bun', color: 'sandybrown' },
+  { name: 'Gluten Free Bun', color: 'peru' },
+  { name: 'Lettuce Wrap', color: 'olivedrab' },
+  { name: 'Beef Patty', color: '#3F250B' },
+  { name: 'Soy Patty', color: '#3F250B' },
+  { name: 'Black Bean Patty', color: '#3F250B' },
+  { name: 'Chicken Patty', color: 'burlywood' },
+  { name: 'Lettuce', color: 'lawngreen' },
+  { name: 'Tomato', color: 'tomato' },
+  { name: 'Bacon', color: 'maroon' },
+  { name: 'Onion', color: 'lightyellow' },
+  { name: 'Cheddar Cheese', color: '#FDE18B' },
+  { name: 'Swiss Cheese', color: '#F1E1A8' },
+];
+
+const App = () => {
+  const [stack, setStack] = useState([]); // Initialize stack state
+
+  // Function to add an ingredient to the burger stack
+  const addToBurger = (ingredient) => {
+    setStack([...stack, ingredient]);
+  };
+
+  // Function to remove an ingredient from the burger stack
+  const removeFromBurger = (index) => {
+    const newStack = stack.filter((_, i) => i !== index);
+    setStack(newStack);
+  };
+
+  return (
+    <main>
+      <h1>Burger Stacker</h1>
+      <section>
+        <IngredientList ingredients={availableIngredients} addIngredient={addToBurger} />
+        <BurgerStack stack={stack} removeIngredient={removeFromBurger} />
+      </section>
+    </main>
+  );
+};
+
+export default App;
